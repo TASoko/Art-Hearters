5742d709c4cfd6b2e3a745851813fe7c5022890a
J-Boogie <jackr353@gmail.com>
2021-04-07 16:19:37 -0500

    add files to mirror file structure of activity 21.4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-//Go over to make sure everything mathes//
+//Go over to make sure everything matches//
 const express = require("express");
 
 const mongoose = require("mongoose");
@@ -7,9 +7,19 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// jack's additions
+const passport = require("passport")
+const passportLocal = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const expressSession = require("express-session");
+
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
