bdfdcc8fddcf660350503b1533dff41a0a898f34
Thoko <thokozile.soko@yahoo.com>
2021-04-15 16:27:18 -0400

    I've created a signup page and made routes for the login and signup. Need to work on the home/landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import './App.css';
+import Login from './pages/login';
 import Signup from './pages/signup';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
 
-  return (
-    <div className="App">
-      <Signup />
-    </div>
-  );
+
+
+function App () {
+
+    return (
+      <Router basemname={`/${process.env.PUBLIC_URL}`}>
+        <div className="main">
+          {/* <Route exact path="/" component={LandingPage} /> */}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </div>
+      </Router>
+    );
 
 }
 
