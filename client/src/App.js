7359a29c719aa575bf5d6e8ca168bd24e81bf593
Thoko <thokozile.soko@yahoo.com>
2021-04-15 17:49:47 -0400

    I made an explore page which will be the dashboard people see if they aren't logged in. It's somehow connected to the home page as is using those properties so I'll have to fix it.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Home from './pages/home';
+import Explore from './pages/explore';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
@@ -16,6 +17,7 @@ function App () {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/explore" component={Explore} />
         </div>
       </Router>
     );
