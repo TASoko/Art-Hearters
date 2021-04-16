f3ac58e35317670a48dfd45170378b8c60247092
Thoko <thokozile.soko@yahoo.com>
2021-04-15 20:07:41 -0400

    deploying

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+  process.env.MONGODB_URI || "mongodb://localhost/limitless-cliffs-88318",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
 );
 
 // Start the API server
