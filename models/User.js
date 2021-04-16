df96f289a1378a24b6cb5559726a18c74f353fb9
Thoko <thokozile.soko@yahoo.com>
2021-04-15 21:21:07 -0400

    reorganizing files yet again

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		date: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
+
+// Virtual Populate
+UserSchema.virtual("projects", {
+	ref: "project",
+	foreignField: "user",
+	localField: "_id",
+});
+UserSchema.virtual("jobs", {
+	ref: "job",
+	foreignField: "user",
+	localField: "_id",
+});
+UserSchema.virtual("events", {
+	ref: "event",
+	foreignField: "user",
+	localField: "_id",
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
