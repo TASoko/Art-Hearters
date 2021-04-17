5cadb75463c18000279603b5e87020f4089486a9
Thoko <thokozile.soko@yahoo.com>
2021-04-17 13:26:29 -0400

    I've made the logged in dashboard. I've got almost everything but I need to reposition the image so that it's inline with the buttons.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/login';
 import Signup from './pages/signup';
 import Home from './pages/home';
 import Explore from './pages/explore';
+import LoggedIn from './pages/loggedin';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
@@ -18,6 +19,7 @@ function App () {
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/explore" component={Explore} />
+          <Route exact path="/id" component={LoggedIn} />
         </div>
       </Router>
     );
