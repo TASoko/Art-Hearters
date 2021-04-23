43b24815bcc73803f763cab2b9a80c2676e31ece
J-Boogie <jackr353@gmail.com>
2021-04-23 15:24:30 -0500

    job seeds in but seedDB not functional yet

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -15,7 +15,8 @@ const jobSeed = [
     description: "In search of a competent team manager who can handle a rush. Looking for a dedicated go-getter who is punctual and detail-oriented. 3+ years in restaurant management preferred.",
     from: "FROM",
     to: "TO",
-    date: new Date(Date.now())
+    date: new Date(Date.now()),
+    aws_image_url: "https://wjr-bucket-1.s3.us-east-2.amazonaws.com/thokos-brewhouse.jpeg"
   },
   {
     user: "USER",
@@ -25,14 +26,30 @@ const jobSeed = [
     description: "In need of an experienced bartender with a creative approach to the job. Come ready to experiment with cocktail recipes. Must perform under pressure and scrutiny from our long-time returning clients. Vincent’s is dedicated to providing exceptional service and we expect the highest degree of dedication from our bartenders. ",
     from: "FROM",
     to: "TO",
-    date: new Date(Date.now())
+    date: new Date(Date.now()),
+    aws_image_url:"https://wjr-bucket-1.s3.us-east-2.amazonaws.com/vincents.jpeg"
   },
   {
-    title: "Another One",
-    author: "admin",
-    body:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    date: new Date(Date.now())
+    user: "USER",
+    team: "Fat Cat Flowers",
+    location: "Lower East Side",
+    position: "Arranger",
+    description: "Looking for a talented and passionate flower arranger with 5+ years of experience. Our small team of dedicated florists needs one more to join the ranks. Serious applicants will be ready to engage in a work culture of honest feedback and grit. No softies. Experience in bouquets is a plus.",
+    from: "FROM",
+    to: "TO",
+    date: new Date(Date.now()),
+    aws_image_url:"https://wjr-bucket-1.s3.us-east-2.amazonaws.com/fat-cat-flowers.jpeg"
+  },
+  {
+    user: "USER",
+    team: "Jack's Tape Dispensary",
+    location: "Brooklyn",
+    position: "Sales Representative",
+    description: "Do you have a passion for adhesives? If so, apply today! Become a sales representative at Jack’s Tape Dispensary. Duct, painter’s, double-sided, scotch, masking, electrical--we have it all! Join our team as someone who is passionate about getting the right thing to stick. We also keep an expansive stock of clips, bands, and glues. ",
+    from: "FROM",
+    to: "TO",
+    date: new Date(Date.now()),
+    aws_image_url:"https://wjr-bucket-1.s3.us-east-2.amazonaws.com/jacks-tape-dispensary.jpeg"
   }
 ];
 
