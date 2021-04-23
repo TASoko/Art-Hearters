e03b654530d81188f56ed97674120bba73f4a018
J-Boogie <jackr353@gmail.com>
2021-04-23 17:25:34 -0500

    seeding seems to be working, making a pull request for the latest updates on my end

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// // This file empties the Posts collection and inserts the books below
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
-
 // Connect to the Mongo DB
 mongoose.connect(
 	process.env.MONGODB_URI || "mongodb://localhost/limitless-cliffs-88318",
