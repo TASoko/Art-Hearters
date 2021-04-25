871d2b359e89c292bf6fa534ca56a2196c159c5d
Veronika Balakhovsky <vbalakhovsky32@gmail.com>
2021-04-25 19:37:33 -0400

    populated modal

--- client/src/components/Card/jobcard.js
+++ client/src/components/Card/jobcard.js
@@ -20,7 +20,7 @@ function JobCard({ data }) {
 					</div>
 				</div>
 			</div>
-			{showModal ? (
+            {showModal ? (
 				<>
 					<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
 						<div className='relative w-auto my-6 mx-auto max-w-3xl'>
@@ -28,7 +28,7 @@ function JobCard({ data }) {
 							<div className='modal border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
 								{/*header*/}
 								<div className='flex items-start justify-between p-5 border-b border-dotted border-blueGray-200 rounded-t'>
-									<h3 className='text-3xl font-semibold'>Modal Title</h3>
+									<h3 className='text-3xl font-semibold'>{data?.job}</h3>
 									<button
 										className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
 										onClick={() => setShowModal(false)}
@@ -41,17 +41,13 @@ function JobCard({ data }) {
 								{/*body*/}
 								<div className='relative p-6 flex-auto'>
 									<p className='my-4 text-blueGray-500 text-lg leading-relaxed'>
-										Location:{}
+										Location: {data?.location}
 									</p>
 									<p className='my-4 text-blueGray-500 text-lg leading-relaxed'>
-										From:{} To:{}
+										From: {data?.from} To: {data?.to}
 									</p>
 									<p className='my-4 text-blueGray-500 text-lg leading-relaxed'>
-										I always felt like I could do anything. That’s the main
-										thing people are controlled by! Thoughts- their perception
-										of themselves! They're slowed down by their perception of
-										themselves. If you're taught you can’t do anything, you
-										won’t do anything. I was taught I could do everything.
+										{data?.description}
 									</p>
 								</div>
 								{/*footer*/}
@@ -63,13 +59,13 @@ function JobCard({ data }) {
 									>
 										Close
 									</button>
-									<button
+									{/* <button
 										className='bg-emerald-500 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
 										type='button'
 										onClick={() => setShowModal(false)}
 									>
 										Add
-									</button>
+									</button> */}
 								</div>
 							</div>
 						</div>
