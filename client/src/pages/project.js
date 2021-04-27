85329662e08e8c35d0dddb6a9ac2ab659e6bae06
Veronika Balakhovsky <vbalakhovsky32@gmail.com>
2021-04-27 14:05:43 -0400

    added go back button in events, projects, jobs

--- client/src/pages/project.js
+++ client/src/pages/project.js
@@ -418,6 +418,14 @@ function Project() {
 				</button>
 			)}
 
+<button
+					type='button'
+					className='goBack login py-2 px-4 mb-10  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-  transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2'
+					onClick={() => window.history.back()}
+				>
+					Go Back
+				</button>
+
 			<h1>All Projects</h1>
 			<hr />
 
